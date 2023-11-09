0255be0b42ac2ff155e1a429fdf468c639d8b66f
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-11-09 12:28:27 -0600

    Update program.py

--- program.py
+++ program.py
@@ -8,6 +8,15 @@ selection = -1
 #class menu:
 
 
+def readfile(file_loc):
+	list = []
+	file = open(file_loc, "r")
+	lines = file.readlines()
+	for line in lines:
+		list.append(line):
+	
+	return list
+
 red_text = lambda string: u"\u001b[31m" + string + u"\u001b[0m"
 blue_text = lambda string: u"\u001b[34m" + string + u"\u001b[0m"
 green_text = lambda string: u"\u001b[32m" + string + u"\u001b[0m"
@@ -18,8 +27,7 @@ green_back = lambda string: u"\u001b[42m" + string + u"\u001b[0m"
 
 
 string = "hello"
-text = 
-list = ""
+text = ""
 while(selection != "end"):
 	os.system('cls')
 	print("1. Create a list\n2. Load a list\n")
